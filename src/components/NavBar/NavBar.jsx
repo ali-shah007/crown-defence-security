c8c263a8a32f6368803a2f55ce60bd9d0f1c337a
ali-shah007 <alishah69.ah@gmail.com>
2024-06-07 03:57:05 +0500

    aa

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '../../assets/logo.jpg';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   const data = [
@@ -31,20 +31,26 @@ function NavBar() {
       <div className='lg:hidden flex w-full h-[120px] bg-black text-white'>
         <div className='flex w-[65vw] h-[120px] mx-auto px-4'>
           <div className='flex justify-center items-center w-full'>
-          <Link to='/'> <img className='w-[286px] h-[102px] hover:scale-105 duration-200 rounded-lg' src={logo} alt='logo' /> </Link>
+            <NavLink to='/'> <img className='w-[286px] h-[102px] hover:scale-105 duration-200 rounded-lg' src={logo} alt='logo' /> </NavLink>
           </div>
         </div>
       </div>
       <div className='flex w-full h-[102px] bg-black lg:bg-transparent text-white lg:fixed z-10 backdrop-blur-3xl border-b border-white'>
         <div className='hidden lg:flex justify-end items-center pr-5 w-1/3 border-r border-white'>
-          <Link to='/'><img className='w-[200px] h-auto hover:scale-105 duration-200 rounded-lg' src={logo} alt='logo' /></Link>
+          <NavLink to='/'><img className='w-[200px] h-auto hover:scale-105 duration-200 rounded-lg' src={logo} alt='logo' /></NavLink>
         </div>
         <div className='flex w-[100vw] lg:w-2/3 text-white mx-auto px-4 justify-center items-center'>
           <ul className='flex gap-10 lg:gap-20 text-base capitalize font-bold'>
             {data.map(({ id, title, tolink }) => (
-              <Link to={tolink} key={id} className='cursor-pointer hover:scale-105 duration-200 hover:text-[#c6af6b]'>
+              <NavLink
+                to={tolink}
+                key={id}
+                className={({ isActive }) =>
+                  `cursor-pointer hover:scale-105 duration-200 lg:hover:text-[#c6af6b] ${isActive ? 'bg-[#c6af6b] text-white p-2 rounded' : ''}`
+                }
+              >
                 {title}
-              </Link>
+              </NavLink>
             ))}
           </ul>
         </div>
