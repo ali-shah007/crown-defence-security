1046492e1ee1aac86d66ed006c8af1636e2d0f50
ali-shah007 <alishah69.ah@gmail.com>
2024-06-07 03:30:33 +0500

    aa

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -31,12 +31,12 @@ function NavBar() {
       <div className='lg:hidden flex w-full h-[120px] bg-black text-white'>
         <div className='flex w-[65vw] h-[120px] mx-auto px-4'>
           <div className='flex justify-center items-center w-full'>
-            <img className='w-[286px] h-[102px]' src={logo} alt='logo' />
+          <Link to='/'> <img className='w-[286px] h-[102px] hover:scale-105 duration-200 rounded-lg' src={logo} alt='logo' /> </Link>
           </div>
         </div>
       </div>
       <div className='flex w-full h-[102px] bg-black lg:bg-transparent text-white lg:fixed z-10 backdrop-blur-3xl border-b border-white'>
-        <div className='flex justify-end items-center pr-5 w-1/3 border-r border-white'>
+        <div className='hidden lg:flex justify-end items-center pr-5 w-1/3 border-r border-white'>
           <Link to='/'><img className='w-[200px] h-auto hover:scale-105 duration-200 rounded-lg' src={logo} alt='logo' /></Link>
         </div>
         <div className='flex w-[100vw] lg:w-2/3 text-white mx-auto px-4 justify-center items-center'>
