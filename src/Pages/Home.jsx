de317904630c143638dc64b201af16c6026af9e0
ali-shah007 <alishah69.ah@gmail.com>
2024-06-12 07:06:18 +0500

    new

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero/Hero'
 import Featured from '../components/Featured/Featured'
 import Marquee from '../components/Marquee/Marquee'
 import CallUs from '../components/CallUs/CallUs'
+import Brochure from '../components/Brochure/Brochure'
 
 
 function Home() {
@@ -12,6 +13,8 @@ function Home() {
         <Marquee/>
         <CallUs/>
         <Featured/>
+        <Brochure/>
+        
     </div>
   )
 }
--- src/components/Brochure/Brochure.jsx
+++ src/components/Brochure/Brochure.jsx
@@ -1,27 +1,71 @@
-// src/Brochure.js
 import React from 'react';
 import HTMLFlipBook from 'react-pageflip';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import './Brochure.css';
 
 const Brochure = () => {
+  const flipBookRef = React.createRef();
+
+  const handleNextPage = () => {
+    flipBookRef.current.pageFlip().flipNext();
+  };
+
+  const handlePrevPage = () => {
+    flipBookRef.current.pageFlip().flipPrev();
+  };
+
   return (
-    <div className="hidden lg:flex brochure-container">
-      <HTMLFlipBook width={500} height={600} showCover={true}>
-        <div className="demoPage">
-        <img className=' w-full h-full object-cover' src="https://images.pexels.com/photos/430208/pexels-photo-430208.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Description" />
+    <div className="bg-[#343A40] brochure-container ">
+      <HTMLFlipBook
+        width={300}
+        height={450}
+        size="stretch"
+        minWidth={300}
+        minHeight={450}
+        maxWidth={500}
+        maxHeight={750}
+        ref={flipBookRef}
+        className="brochure "
+      >
+        <div className="page" style={{ WebkitUserDrag: 'element' }}>
+          <img className='w-full h-full object-cover' src="https://www.leisureguardsecurity.co.uk/tower-guard/files/mobile/2.jpg" alt="" />
         </div>
-        <div className="demoPage">
-          <img className=' w-full h-full object-cover' src="https://images.pexels.com/photos/96612/pexels-photo-96612.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Description" />
-          
+        <div className="page" style={{ WebkitUserDrag: 'element' }}>
+          <img className='w-full h-full object-cover' src="https://www.leisureguardsecurity.co.uk/tower-guard/files/mobile/3.jpg" alt="" />
         </div>
-        <div className="demoPage">
-        <img className=' w-full h-full object-cover' src="https://images.pexels.com/photos/1119152/pexels-photo-1119152.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Description" />
+        <div className="page" style={{ WebkitUserDrag: 'element' }}>
+          <img className='w-full h-full object-cover' src="https://www.leisureguardsecurity.co.uk/tower-guard/files/mobile/4.jpg" alt="" />
         </div>
-        <div className="demoPage">
-        <img className=' w-full h-full object-cover' src="https://images.pexels.com/photos/68798/pexels-photo-68798.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Description" />
+        <div className="page" style={{ WebkitUserDrag: 'element' }}>
+          <img className='w-full h-full object-cover' src="https://www.leisureguardsecurity.co.uk/tower-guard/files/mobile/5.jpg" alt="" />
         </div>
-        {/* Add more pages as needed */}
+        <div className="page" style={{ WebkitUserDrag: 'element' }}>
+          <img className='w-full h-full object-cover' src="https://www.leisureguardsecurity.co.uk/tower-guard/files/mobile/6.jpg" alt="" />
+        </div>
+        <div className="page" style={{ WebkitUserDrag: 'element' }}>
+          <img className='w-full h-full object-cover' src="https://www.leisureguardsecurity.co.uk/tower-guard/files/mobile/7.jpg" alt="" />
+        </div>
+        <div className="page" style={{ WebkitUserDrag: 'element' }}>
+          <img className='w-full h-full object-cover' src="https://www.leisureguardsecurity.co.uk/tower-guard/files/mobile/8.jpg" alt="" />
+        </div>
+        <div className="page" style={{ WebkitUserDrag: 'element' }}>
+          <img className='w-full h-full object-cover' src="https://www.leisureguardsecurity.co.uk/tower-guard/files/mobile/9.jpg" alt="" />
+        </div>
+        <div className="page" style={{ WebkitUserDrag: 'element' }}>
+          <img className='w-full h-full object-cover' src="https://www.leisureguardsecurity.co.uk/tower-guard/files/mobile/10.jpg" alt="" />
+        </div>
+        <div className="page" style={{ WebkitUserDrag: 'element' }}>
+          <img className='w-full h-full object-cover' src="https://www.leisureguardsecurity.co.uk/tower-guard/files/mobile/11.jpg" alt="" />
+        </div>
       </HTMLFlipBook>
+      <div className="navigation-buttons">
+        <button onClick={handlePrevPage} className="prev-button">
+          <FaArrowLeft size={30} />
+        </button>
+        <button onClick={handleNextPage} className="next-button">
+          <FaArrowRight size={30} />
+        </button>
+      </div>
     </div>
   );
 };
